8e66356775a02c99f50472065cfb91a668a4f462
uuu <498801920@qq.com>
2020-05-25 09:43:25 +0200

    convert to string

---
+++ netværkprog/netværkprog/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+namespace Netværk_Programmering
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            string text1 = "ÆØÅLOLMAOÆØÅ";
+            var bytes1 = Encoding.ASCII.GetBytes(text1);
+
+            Console.WriteLine(text1);
+
+            text1 = Encoding.ASCII.GetString(bytes1);
+
+            string text2 = "Hello World! ÆØÅ ÆØÅ LÆØÅ YE";
+            var bytes = Encoding.UTF8.GetBytes(text2);
+
+            Console.WriteLine(bytes);
+
+            text2 = Encoding.UTF8.GetString(bytes);
+
+            Console.WriteLine(text2);
+
+            
+
+        }
+    }
+}
+
