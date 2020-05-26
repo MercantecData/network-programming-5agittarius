9b06745d3003c20ba732f4aaaae6d16304b5e782
uuu <498801920@qq.com>
2020-05-26 10:57:58 +0200

    client

---
+++ client/client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+
+
+,,,
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 2069;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            client.Close();
+        }
+    }
+}
